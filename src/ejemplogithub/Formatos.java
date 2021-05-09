84204ac3a90672e87fbe0bd2531dc690f752ee06
oqekr <oqekr@CerealKiller22>
2021-05-08 20:34:22 -0600

    Confirmacion ejemplo

--- src/ejemplogithub/Formatos.java
+++ src/ejemplogithub/Formatos.java
@@ -16,11 +16,11 @@ public class Formatos {
      */
     public static void main(String[] args) {
       String cabecera = "\n\tPronostico de clima: \n";
-      cabecera+="\n\tdia\t\tmañana\tnoche\tcondiciones\n";
-      cabecera+="\t----\t\t-----\t-------\t--------\n";
-      String pronostico="\tDomingo\t25c\t\t33c\t\tSoleado\n";
-      pronostico+="\tLunes\t18c\t\t19c\t\tNublado\n";
-       pronostico+="\tMartes\t40c\t\t29c\t\tcaluroso\n";
+      cabecera+="\n\tdia\tmañana\tnoche\tcondiciones\n";
+      cabecera+="\t------\t-------\t-------\t-------\n";
+      String pronostico="\tDomingo\t25c\t33c\tSoleado\n";
+      pronostico+="\tLunes\t18c\t19c\tNublado\n";
+       pronostico+="\tMartes\t40c\t29c\tcaluroso\n";
         System.out.println(cabecera+pronostico);
     }
     
